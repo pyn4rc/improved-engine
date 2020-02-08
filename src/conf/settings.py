ca8417fd7ceaecce0371c3036b89bf6a1b6ed59b
pyn4rc <pyn4rc@gmail.com>
2020-02-08 06:13:07 +0300

    dev

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -1,8 +1,12 @@
 import os
 
+from rhea import Rhea
+
+rhea_config = Rhea.read_configs(os.environ)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '%=#_jk*1e_!ml72z9k)s5=66myqc$*g8(hi#0l4enlir@1a5el'
-DEBUG = True
+SECRET_KEY = rhea_config.get_string('SECRET_KEY')
+DEBUG = rhea_config.get_boolean('DEBUG', is_optional=True, default=False)
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,8 +44,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': rhea_config.get_string('POSTGRES_DB'),
+        'USER': rhea_config.get_string('POSTGRES_USER'),
+        'PASSWORD': rhea_config.get_string('POSTGRES_PASSWORD'),
+        'HOST': rhea_config.get_string('POSTGRES_HOST'),
+        'PORT': rhea_config.get_string('POSTGRES_PORT'),
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
@@ -50,9 +58,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ru'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
